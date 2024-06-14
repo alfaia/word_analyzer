9d8653a7b06f687f4a4f37b4671ac1a1e4609af8
alfaia <rd.alfaia@gmail.com>
2024-06-14 02:29:36 -0300

    the structure of lint and test in the yml file was changed

--- word_analyzer/tests.py
+++ word_analyzer/tests.py
@@ -1,34 +1,35 @@
-from django.test import TestCase
+import pytest
 from django.urls import reverse
 from rest_framework import status
+from pytest_django.asserts import assertJSONEqual
 
 
-class APITests(TestCase):
+@pytest.mark.django_db
+class TestAPITests:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.client = Client()
+
     def test_vowel_count(self):
         url = reverse('vowel-count')  # Usando o nome da URL 'vowel-count'
         data = {'words': ['batman', 'robin', 'coringa']}
-        response = self.client.post(
-            url, data, format='json', content_type='application/json'
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            response.data, {'batman': 2, 'robin': 2, 'coringa': 3}
-        )
+        response = self.client.post(url, data, format='json', content_type='application/json')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assertJSONEqual(response.content, {'batman': 2, 'robin': 2, 'coringa': 3})
 
     def test_sort_asc(self):
         url = reverse('sort-words')  # Usando o nome da URL 'sort-words'
         data = {'words': ['batman', 'robin', 'coringa'], 'order': 'asc'}
-        response = self.client.post(
-            url, data, format='json', content_type='application/json'
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, ['batman', 'coringa', 'robin'])
+        response = self.client.post(url, data, format='json', content_type='application/json')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assertJSONEqual(response.content, ['batman', 'coringa', 'robin'])
 
     def test_sort_desc(self):
         url = reverse('sort-words')  # Usando o nome da URL 'sort-words'
         data = {'words': ['batman', 'robin', 'coringa'], 'order': 'desc'}
-        response = self.client.post(
-            url, data, format='json', content_type='application/json'
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, ['robin', 'coringa', 'batman'])
+        response = self.client.post(url, data, format='json', content_type='application/json')
+        
+        assert response.status_code == status.HTTP_200_OK
+        assertJSONEqual(response.content, ['robin', 'coringa', 'batman'])
