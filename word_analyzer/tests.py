5b1eea8ed89fda2964d4b733c9ee8dfa08d9c3ea
alfaia <rd.alfaia@gmail.com>
2024-06-14 02:30:57 -0300

    adjusting lint error

--- word_analyzer/tests.py
+++ word_analyzer/tests.py
@@ -13,23 +13,31 @@ class TestAPITests:
     def test_vowel_count(self):
         url = reverse('vowel-count')  # Usando o nome da URL 'vowel-count'
         data = {'words': ['batman', 'robin', 'coringa']}
-        response = self.client.post(url, data, format='json', content_type='application/json')
-        
+        response = self.client.post(
+            url, data, format='json', content_type='application/json'
+        )
+
         assert response.status_code == status.HTTP_200_OK
-        assertJSONEqual(response.content, {'batman': 2, 'robin': 2, 'coringa': 3})
+        assertJSONEqual(
+            response.content, {'batman': 2, 'robin': 2, 'coringa': 3}
+        )
 
     def test_sort_asc(self):
         url = reverse('sort-words')  # Usando o nome da URL 'sort-words'
         data = {'words': ['batman', 'robin', 'coringa'], 'order': 'asc'}
-        response = self.client.post(url, data, format='json', content_type='application/json')
-        
+        response = self.client.post(
+            url, data, format='json', content_type='application/json'
+        )
+
         assert response.status_code == status.HTTP_200_OK
         assertJSONEqual(response.content, ['batman', 'coringa', 'robin'])
 
     def test_sort_desc(self):
         url = reverse('sort-words')  # Usando o nome da URL 'sort-words'
         data = {'words': ['batman', 'robin', 'coringa'], 'order': 'desc'}
-        response = self.client.post(url, data, format='json', content_type='application/json')
-        
+        response = self.client.post(
+            url, data, format='json', content_type='application/json'
+        )
+
         assert response.status_code == status.HTTP_200_OK
         assertJSONEqual(response.content, ['robin', 'coringa', 'batman'])
