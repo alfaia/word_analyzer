6eee2ffa0fb064659f54c3fb7ea5860d701145af
alfaia <rd.alfaia@gmail.com>
2024-06-14 17:09:04 -0300

    adjusting path API

--- setup/urls.py
+++ setup/urls.py
@@ -8,7 +8,7 @@ from word_analyzer.views import ApiRootView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('word_analyzer.urls')),
-    path('/', ApiRootView.as_view(), name='api-word-analyzer')
+    path('api/', ApiRootView.as_view(), name='api-word-analyzer')
 ]
 
 if not settings.DEBUG:
