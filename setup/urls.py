7af91a1dcc60207befb4e31809f26e584526bac1
alfaia <rd.alfaia@gmail.com>
2024-06-14 15:40:22 -0300

    duplicate path was removed in urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.urls import path, re_path
+from django.urls import re_path
 from django.views.static import serve
 from django.contrib import admin
 from django.urls import path, include
